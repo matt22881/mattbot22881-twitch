1cf3c0fa0f2dc4232fcd2a03945ad96b188eaabe
matt22881 <matt22881@gmail.com>
2021-05-03 03:57:37 -0400

    add titcmd

--- src/commands/titcmd.js
+++ src/commands/titcmd.js
@@ -1,22 +1,21 @@
-import gaim from './aim'
+import gaim from './gaim.js'
 
 
 
 exports.run = (client, message, args, user, channel, self) => {
-  gaim.run(client, message, args, user, channel, self)
-  setTimeout(() => {client.say(channel, `!quad`)},1500)
-  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},3000)
-  setTimeout(() => {client.say(channel, `!tyt`)},8000)
-  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},9500)
-  setTimeout(() => {client.say(channel, `!teamyt`)},14500)
-  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},16000)
-  setTimeout(() => {client.say(channel, `!discord`)},21000)
-  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},22500)
-  setTimeout(() => {client.say(channel, `!quad`)},27500)
-  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},29000)
-  setTimeout(() => {client.say(channel, `!quad`)},33000)
-  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},345000)
-  setTimeout(() => {client.say(channel, `!quad`)},395000)
-  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},410000)
-
+  gaim.run(client, message, args, user, channel, self);
+  setTimeout(() => {client.say(channel, `!quad`)},2000);
+  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},4000);
+  setTimeout(() => {client.say(channel, `!tyt`)},9000);
+  setTimeout(() => {client.say(channel, `!ttok`)},11000);
+  setTimeout(() => {client.say(channel, `!tig`)},13000);
+  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},15000);
+  setTimeout(() => {client.say(channel, `!discord`)},20000);
+  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},22000);
+  setTimeout(() => {client.say(channel, `!teamyt`)},27000);
+  setTimeout(() => {client.say(channel, `!teamig`)},29000);
+  setTimeout(() => {gaim.run(client, message, args, user, channel, self)},31000);
+  // setTimeout(() => {gaim.run(client, message, args, user, channel, self)},345000);
+  // setTimeout(() => {gaim.run(client, message, args, user, channel, self)},29000);
+  // setTimeout(() => {gaim.run(client, message, args, user, channel, self)},16000);
 }
